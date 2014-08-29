9d6d8278bb84a3d99fc7474cab2d2aceb60c3434
robneu <rob@wpsitecare.com>
2014-08-29 16:07:05 -0500

    Switched to brick and changed font stack

--- compass/includes/scripts.php
+++ compass/includes/scripts.php
@@ -10,11 +10,11 @@
  * @since       1.0.0
  */
 
-add_action( 'wp_enqueue_scripts', 'compass_scripts' );
+add_action( 'wp_enqueue_scripts', 'compass_enqueue_styles' );
 /**
  * Enqueue scripts and styles.
  */
-function compass_scripts() {
+function compass_enqueue_styles() {
 	$theme = wp_get_theme();
-	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:300,300italic,400,600,700', array(), $theme->get( 'Version' ) );
+	wp_enqueue_style( 'brick-fonts', '//brick.a.ssl.fastly.net/Raleway:400,600/Clear+Sans:300,300i,600,800', array(), $theme->get( 'Version' ) );
 }
