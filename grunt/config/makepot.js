72138970deed4e35b4c48a91c00278721d470812
Gary Jones <gary@garyjones.co.uk>
2014-09-04 12:52:57 +0100

    Exclude Hybrid Core from Poedit re-scans
    Needs testing. Might need a slightly different relative path.

--- grunt/config/makepot.js
+++ grunt/config/makepot.js
@@ -6,18 +6,19 @@ module.exports = function( grunt ) {
 				cwd: '<%= paths.theme %>',
 				domainPath: '<%= paths.languages %>',
 				processPot: function( pot ) {
-					pot.headers['report-msgid-bugs-to']   = grunt.config( 'pkg.pot.reportmsgidbugsto' );
-					pot.headers['last-translator']        = grunt.config( 'pkg.pot.lasttranslator' );
-					pot.headers['language-team']          = grunt.config( 'pkg.pot.languageteam' );
-					pot.headers['plural-forms']           = grunt.config( 'pkg.pot.pluralforms' );
-					pot.headers['x-poedit-basepath']      = grunt.config( 'pkg.pot.xpoeditbasepath' );
-					pot.headers['x-poedit-language']      = grunt.config( 'pkg.pot.xpoeditlanguage' );
-					pot.headers['x-poedit-country']       = grunt.config( 'pkg.pot.xpoeditcountry' );
-					pot.headers['x-poedit-sourcecharset'] = grunt.config( 'pkg.pot.xpoeditsourcecharset' );
-					pot.headers['x-poedit-keywordslist']  = grunt.config( 'pkg.pot.xpoeditkeywordslist' );
-					pot.headers['x-poedit-bookmarks']     = grunt.config( 'pkg.pot.xpoeditbookmarks' );
-					pot.headers['x-poedit-searchpath-0']  = grunt.config( 'pkg.pot.xpoeditsearchpath0' );
-					pot.headers['x-textdomain-support']   = grunt.config( 'pkg.pot.xtextdomainsupport' );
+					pot.headers['report-msgid-bugs-to']          = grunt.config( 'pkg.pot.reportmsgidbugsto' );
+					pot.headers['last-translator']               = grunt.config( 'pkg.pot.lasttranslator' );
+					pot.headers['language-team']                 = grunt.config( 'pkg.pot.languageteam' );
+					pot.headers['plural-forms']                  = grunt.config( 'pkg.pot.pluralforms' );
+					pot.headers['x-poedit-basepath']             = grunt.config( 'pkg.pot.xpoeditbasepath' );
+					pot.headers['x-poedit-language']             = grunt.config( 'pkg.pot.xpoeditlanguage' );
+					pot.headers['x-poedit-country']              = grunt.config( 'pkg.pot.xpoeditcountry' );
+					pot.headers['x-poedit-sourcecharset']        = grunt.config( 'pkg.pot.xpoeditsourcecharset' );
+					pot.headers['x-poedit-keywordslist']         = grunt.config( 'pkg.pot.xpoeditkeywordslist' );
+					pot.headers['x-poedit-bookmarks']            = grunt.config( 'pkg.pot.xpoeditbookmarks' );
+					pot.headers['x-poedit-searchpath-0']         = grunt.config( 'pkg.pot.xpoeditsearchpath0' );
+					pot.headers['x-poedit-searchpathexcluded-0'] = grunt.config( 'pkg.pot.xpoeditsearchpathexcluded0' );
+					pot.headers['x-textdomain-support']          = grunt.config( 'pkg.pot.xtextdomainsupport' );
 					return pot;
 				},
 				type: 'wp-theme'
