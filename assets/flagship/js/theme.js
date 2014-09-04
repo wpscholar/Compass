2c7e9594cda42026f0988e81ee968d1668b0e0bb
Gary Jones <gary@garyjones.co.uk>
2014-09-04 17:38:06 +0100

    Fix code standards for JS
    (JS is still broken at this point)

--- assets/flagship/js/theme.js
+++ assets/flagship/js/theme.js
@@ -15,18 +15,20 @@ window.compass = window.compass || {};
 
 		//* Skip Link Focus Fix
 		skipLinks: function() {
-			var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-				is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-				is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+			var eventMethod,
+				isWebkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+				isOpera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+				isIe     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
-			if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
-				var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+			if ( ( isWebkit || isOpera || isIe ) && 'undefined' !== typeof( document.getElementById ) ) {
+				eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
 				window[ eventMethod ]( 'hashchange', function() {
 					var element = document.getElementById( location.hash.substring( 1 ) );
 
 					if ( element ) {
-						if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+						if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
 							element.tabIndex = -1;
+						}
 
 						element.focus();
 					}
@@ -35,53 +37,99 @@ window.compass = window.compass || {};
 		},
 
 		//* Navigation Tab Fix
-		navTabbing: function( a ) {
-			var b, c, d = 'menu-item-hover',
-				e = function() {
-					b = a( '#menu-after-header' ),
-					b.before( '<button id="js-menu-toggle" class="menu-primary-toggle">Menu</button>' ),
-					c = a( '#js-menu-toggle' ),
-					c.attr({
-						'aria-controls': 'menu-primary-navigation',
-						'aria-expanded': 'false'
-					}),
-					c.on( 'click.dt', function() {
-						'true' === a(this).attr( 'aria-expanded' ) ? g() : f()
-					})
-				},
-				f = function() {
-					b.slideDown(), c.attr({
-						'aria-expanded': 'true'
-					})
-				},
-				g = function() {
-					b.slideUp(), c.attr({
-						'aria-expanded': 'false'
-					})
-				},
-				h = function() {
-					a(this).addClass(d)
-				},
-				i = function() {
-					a(this).delay( '250' ).removeClass(d)
-				},
-				j = function() {
-					a(this).parents( '.menu-item' ).toggleClass(d)
-				},
-				k = function() {
-					e(), a( '.menu li' ).on( 'mouseenter.dt', h).on( 'mouseleave.dt', i).find( 'a' ).on( 'focus.dt blur.dt', j )
-				};
+		navTabbing: function( $ ) {
+			var $primaryNav, $button, hoverClass = 'menu-item-hover',
+
+			/**
+			 * Add in responsive menu feature.
+			 *
+			 * @since 1.0.0
+			 */
+			responsiveMenu = function() {
+				$primaryNav = $( '#menu-primary-navigation' );
+				$primaryNav.before( '<button id="js-menu-toggle" class="menu-primary-toggle">Menu</button>' );
+				$button = $( '#js-menu-toggle' );
+				$button.attr({
+					'aria-controls': 'menu-primary-navigation',
+					'aria-expanded': 'false'
+				});
+
+				$button.on( 'click.incipio', function() {
+					if( 'true' === $(this).attr( 'aria-expanded' ) ) {
+						closeMenu();
+					} else {
+						openMenu();
+					}
+				});
+			},
+
+			openMenu = function() {
+				$primaryNav.slideDown();
+				$button.attr({
+					'aria-expanded': 'true'
+				});
+			},
+
+			closeMenu = function() {
+				$primaryNav.slideUp();
+				$button.attr({
+					'aria-expanded': 'false'
+				});
+			},
+
+			/**
+			 * Add class to menu item on hover.
+			 *
+			 * @since 1.0.0
+			 */
+			menuItemEnter = function() {
+				$( this ).addClass( hoverClass );
+			},
+
+			/**
+			 * After a short delay, remove a class when mouse leaves menu item.
+			 *
+			 * @since 1.0.0
+			 */
+			menuItemLeave = function() {
+				$( this ).delay( '250' ).removeClass( hoverClass );
+			},
+
+			/**
+			 * Toggle menu item class when a link fires a focus or blur event.
+			 *
+			 * @since 1.0.0
+			 */
+			menuItemToggleClass = function() {
+				$( this ).parents( '.menu-item' ).toggleClass( hoverClass );
+			},
+
+			/**
+			 * Fire events on document ready, and bind other events.
+			 *
+			 * @since 1.0.0
+			 */
+			ready = function() {
+				responsiveMenu();
+
+				$( '.menu li' )
+					.on( 'mouseenter.incipio', menuItemEnter )
+					.on( 'mouseleave.incipio', menuItemLeave )
+					.find( 'a' )
+					.on( 'focus.incipio blur.incipio', menuItemToggleClass );
+			};
+
+			// Only expose the ready function to the world
 			return {
-				ready: k
-			}
+				ready: ready
+			};
 		},
 
 		//* Mobile Menu
 		mobileNav: function() {
-
 			$( 'header .nav-menu' ).addClass( 'responsive-menu' ).before( '<div class="responsive-menu-icon"></div>' );
 
-			$( '.responsive-menu-icon' ).click(function(){
+			$( '.responsive-menu-icon' ).click( function() {
 				$( this ).next( 'header .nav-menu' ).slideToggle();
 			});
 
@@ -93,9 +141,10 @@ window.compass = window.compass || {};
 			});
 
 			$( '.responsive-menu > .menu-item' ).click( function( event ) {
-				if (event.target !== this)
-				return;
-					$( this ).find( '.sub-menu:first' ).slideToggle(function() {
+				if (event.target !== this) {
+					return;
+				}
+				$( this ).find( '.sub-menu:first' ).slideToggle(function() {
 					$( this ).parent().toggleClass( 'menu-open' );
 				});
 			});
@@ -109,11 +158,10 @@ window.compass = window.compass || {};
 	});
 
 	// Document ready.
-	jQuery(function() {
+	jQuery(function($) {
 		compass.skipLinks();
-		compass.navTabbing();
+		compass.navTabbing($);
 		compass.mobileNav();
 		compass.loadFitVids();
 	});
-
 })( this, jQuery );
