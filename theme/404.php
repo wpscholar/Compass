0afe27fd6fe0846830bffbe27db242e13162868e
robneu <rob@wpsitecare.com>
2014-09-14 11:53:09 -0500

    Added a template part for 404 errors

--- theme/404.php
+++ theme/404.php
@@ -19,31 +19,7 @@
 
 	<?php tha_content_top(); ?>
 
-	<article class="entry error-404 not-found">
-
-		<header class="entry-header">
-			<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'compass' ); ?></h1>
-		</header><!-- .page-header -->
-
-		<div class="entry-content">
-
-			<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'compass' ); ?></p>
-
-			<?php get_search_form(); ?>
-
-			<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-			<?php
-			/* translators: %1$s: smiley */
-			$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'compass' ), convert_smilies( ':)' ) ) . '</p>';
-			the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2> $archive_content" );
-			?>
-
-			<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-		</div><!-- .entry-content -->
-
-	</article><!-- .error-404 -->
+	<?php get_template_part( 'content/error', '404' ); ?>
 
 	<?php tha_content_bottom(); ?>
 
