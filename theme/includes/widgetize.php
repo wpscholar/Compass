d96e292697b26b543ee538b2eee8db443e263a65
robneu <rob@wpsitecare.com>
2014-09-22 21:52:59 -0500

    Reordered default widget areas
    While it’s nice to have the header right widget area load first, it
    also causes new installs to dump all the default widgets into it rather
    than the primary sidebar. This is really annoying in Genesis and I’d
    like to avoid it.
    
    Also, because the header right area is likely to not have a title, It
    is probably more semantically correct to use a div for widget wrappers
    here.

--- theme/includes/widgetize.php
+++ theme/includes/widgetize.php
@@ -21,16 +21,18 @@ add_action( 'widgets_init', 'compass_register_sidebars', 5 );
 function compass_register_sidebars() {
 	hybrid_register_sidebar(
 		array(
-			'id'          => 'header-right',
-			'name'        => _x( 'Header Right', 'sidebar', 'compass' ),
-			'description' => __( 'The header right sidebar area. It is displayed to the right of the site title area.', 'compass' ),
+			'id'          => 'primary',
+			'name'        => _x( 'Primary Sidebar', 'sidebar', 'compass' ),
+			'description' => __( 'The main sidebar. It is displayed on either the left or right side of the page based on the chosen layout.', 'compass' )
 		)
 	);
 	hybrid_register_sidebar(
 		array(
-			'id'          => 'primary',
-			'name'        => _x( 'Primary Sidebar', 'sidebar', 'compass' ),
-			'description' => __( 'The main sidebar. It is displayed on either the left or right side of the page based on the chosen layout.', 'compass' )
+			'id'            => 'header-right',
+			'name'          => _x( 'Header Right', 'sidebar', 'compass' ),
+			'description'   => __( 'The header right sidebar area. It is displayed to the right of the site title area.', 'compass' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
 		)
 	);
 }
