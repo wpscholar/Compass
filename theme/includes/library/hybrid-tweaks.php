1d9ac51ae0e4c00f08ef8460455b8c99c8726e59
robneu <rob@wpsitecare.com>
2014-09-23 00:31:13 -0500

    Moved non-hybrid functions into another file

--- theme/includes/library/hybrid-tweaks.php
+++ theme/includes/library/hybrid-tweaks.php
@@ -14,7 +14,7 @@
 remove_action( 'wp_head', 'hybrid_meta_template', 1 );
 remove_action( 'wp_head', 'hybrid_doctitle',      0 );
 
-add_action( 'wp_head', 'flagship_doctitle',  0 );
+add_action( 'wp_head', 'flagship_doctitle', 0 );
 /**
  * Add the title to the header with a more SEO-friendly seperator.
  *
@@ -89,97 +89,4 @@ add_filter( 'excerpt_more', 'flagship_seo_excerpt_more' );
  */
 function flagship_seo_excerpt_more( $text ) {
 	return str_replace( 'class="more-link"', 'rel="nofollow" class="more-link"', $text );
-}
-
-/**
- * Sets a common class, `.nav-menu`, for the custom menu widget if used in the
- * header right sidebar.
- *
- * @since  1.0.0
- * @param  array $args Header menu args.
- * @return array $args Modified header menu args.
- */
-function flagship_header_menu_args( $args ) {
-	$args['menu_class'] .= ' nav-menu';
-	return $args;
-}
-
-/**
- * Wrap the header navigation menu in its own nav tags with markup API.
- *
- * @since  1.0.0
- * @param  $menu Menu output.
- * @return string $menu Modified menu output.
- */
-function flagship_header_menu_wrap( $menu ) {
-	return sprintf( '<nav %s>', hybrid_get_attr( 'widget-menu', 'header' ) ) . $menu . '</nav>';
-}
-
-add_action( 'widgets_init', 'flagship_register_footer_widget_areas' );
-/**
- * Register footer widget areas based on the number of widget areas the user
- * wishes to create with `add_theme_support()`.
- *
- * @since  1.0.0
- * @uses   register_sidebar() Register footer widget areas.
- * @return null Return early if there's no theme support.
- */
-function flagship_register_footer_widget_areas() {
-	// Get the current theme's support for footer widgets.
-	$footer_widgets = get_theme_support( 'flagship-footer-widgets' );
-
-	if ( ! $footer_widgets || ! isset( $footer_widgets[0] ) || ! is_numeric( $footer_widgets[0] ) ) {
-		return;
-	}
-
-	$footer_widgets = (int) $footer_widgets[0];
-
-	$counter = 1;
-
-	while ( $counter <= $footer_widgets ) {
-		hybrid_register_sidebar(
-			array(
-				'id'          => sprintf( 'footer-%d', $counter ),
-				'name'        => sprintf( __( 'Footer %d', 'compass' ), $counter ),
-				'description' => sprintf( __( 'Footer %d widget area.', 'compass' ), $counter ),
-			)
-		);
-
-		$counter++;
-	}
-}
-
-add_action( 'tha_footer_before', 'flagship_footer_widgets' );
-/**
- * Displays all registered footer widget areas using a template.
- *
- * @since  1.0.0
- * @uses   locate_template() Load the footer widget template.
- * @return null Return early if there's no theme support.
- */
-function flagship_footer_widgets() {
-	//* Get the current theme's support for footer widgets.
-	$footer_widgets = get_theme_support( 'flagship-footer-widgets' );
-
-	if ( ! $footer_widgets || ! isset( $footer_widgets[0] ) || ! is_numeric( $footer_widgets[0] ) ) {
-		return;
-	}
-
-	//* Check to see if first widget area has widgets. If not, do nothing. No need to check all footer widget areas.
-	if ( ! is_active_sidebar( 'footer-1' ) ) {
-		return;
-	}
-
-	$footer_widgets = (int) $footer_widgets[0];
-
-	$counter = 1;
-
-	echo '<div ' . hybrid_get_attr( 'footer-widgets' ) . '>';
-		echo '<div class="wrap">';
-			while ( $counter <= $footer_widgets ) {
-				include( locate_template( 'sidebar/footer-widgets.php' ) );
-				$counter++;
-			}
-		echo '</div>';
-	echo '</div>';
 }
