a094cf23f1d3e2013fe37cf850134b3504a71052
robneu <rob@wpsitecare.com>
2014-09-24 01:56:35 -0500

    Implemented cleaner inline button method
    I figured out a better way to achieve the same inline search button
    that doesn’t require extra markup. Much better.

--- theme/includes/library/classes/class-search-form.php
+++ theme/includes/library/classes/class-search-form.php
@@ -56,7 +56,7 @@ class Flagship_Search_Form {
 
 	protected function get_button() {
 		return sprintf(
-			'<div class="button-wrap"><button type="submit" aria-label="%s"><span class="screen-reader-text">%s</span></button></div>',
+			'<button type="submit" aria-label="%s"><span class="screen-reader-text">%s</span></button>',
 			esc_attr( apply_filters( 'flagship_search_button_label', __( 'Search', 'compass' ) ) ),
 			apply_filters( 'flagship_search_button_text', __( 'Search', 'compass' ) )
 		);
