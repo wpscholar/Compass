5edbb0375e1252ee0944de4f180eb6840b0d79e8
robneu <rob@wpsitecare.com>
2014-09-24 22:27:09 -0500

    Force theme to support Jetpack - not working
    The cleaner gallery feature breaks the jetpack carousel and tiled
    gallery modules. This should remove support for cleaner gallery when
    those modules are enabled, but for some reason it isn’t working. Need
    to investigate.

--- theme/includes/compatibility.php
+++ theme/includes/compatibility.php
@@ -12,12 +12,28 @@
 
 add_action( 'after_setup_theme', 'compass_jetpack_setup', 15 );
 /**
- * Add theme support for Infinite Scroll.
- * See: http://jetpack.me/support/infinite-scroll/
+ * Make adjustments to the theme when Jetpack is installed and activated.
+ *
+ * @since  1.0.0
+ * @return null if Jetpack isn't active
  */
 function compass_jetpack_setup() {
+	//* Return early if Jetpack isn't enabled.
+	if ( ! class_exists( 'Jetpack' ) ) {
+		return;
+	}
+
+	//* Add support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'footer'    => 'page',
 	) );
+
+	//* Get the active jetpack modules.
+	$modules = get_option( 'jetpack_active_modules' );
+
+	//* Remove cleaner gallery if either Tiled Galleries or Carousel is active.
+	if ( count( array_intersect( array( 'tiled-gallery', 'carousel' ), (array) $modules ) ) > 0 ) {
+		remove_theme_support( 'cleaner-gallery' );
+	}
 }
